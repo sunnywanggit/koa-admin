d4df5bf924a5c6b8deaf402f810f1c73e9d29d17
ergouwang <330035385@qq.com>
2020-06-20 19:49:04 +0800

    '使用模板引擎渲染页面'

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const Koa = require('koa');
 const app = new Koa();
 const koaRouter = require('koa-router')
+const path = require('path')
+const render = require('koa-ejs')
+// remove node_moduless
 
 const router = new koaRouter
 
-router.get('/',async (ctx)=>{
-    ctx.body="首页";
+//配置模板引擎
+render(app,{
+    root:path.join(__dirname,'views'),
+    layout:'layout',
+    viewExt:'html',
+    cache:false,
+    debug:false
 })
+
+//路由跳转
+router.get('/',async ctx=>{
+    await ctx.render('index',{
+        title:'index'
+    })
+})
+
+// router.get('/',async (ctx)=>{
+//     ctx.body="首页";
+// })
 
 /*启动路由*/
 app.use(router.routes());
